fab20800e90dd4fb39bcb709435dafd22a654439
Guilherme Rio-Tinto <guilherme.rio-tinto@kcl.ac.uk>
2022-10-05 12:39:14 +0100

    Add exception

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def checkPrime(num):
+    if num == 2:
+        return True
+    if num > 2:
+        for n in range(2, num):
+            if (num % n) == 0:
+                return False
+        else:
+            return True
+
 def primes(number_of_primes):
+    if(number_of_primes <= 0):
+        raise ValueError("You must enter a value greater than 0")
     list = []
+    n = 2
+    while len(list) != number_of_primes:
+        if checkPrime(n):
+            list.append(n)
+        n +=1
+
     return list
