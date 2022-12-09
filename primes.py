6ddac684ed1d41023d8253aba95da026e7a32f5a
Guilherme Rio-Tinto <guilherme.rio-tinto@kcl.ac.uk>
2022-12-09 08:13:53 +0000

    Final

--- primes.py
+++ primes.py
@@ -1,24 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def checkPrime(num):
     if num == 2:
         return True
-    if num > 2:
-        for n in range(2, num):
-            if (num % n) == 0:
-                return False
-            else:
-                return True
+
+    for i in range(2, int(math.sqrt(num) + 1)):
+        if (num % i == 0):
+            return False
+    return True
 
 def primes(number_of_primes):
     if(number_of_primes <= 0):
-        raise ValueError("You must enter a value greater than 0")
-    list = []
-    n = 2
-    while len(list) != number_of_primes:
-        if checkPrime(n):
-            list.append(n)
-        n +=1
+        raise ValueError("Please enter a number larger than 0")
+        return
 
+    list = []
+    i = 2
+
+    while(len(list) < number_of_primes):
+        if checkPrime(i):
+            list.append(i)
+
+        i += 1
     return list
